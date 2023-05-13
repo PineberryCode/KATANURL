8087acb6614668119578661e8cca4148c0841fff
MINDLUNNY <88601032+PineberryCode@users.noreply.github.com>
2023-05-13 09:44:17 -0500

    Update dev.py

--- src/dev.py
+++ src/dev.py
@@ -63,4 +63,4 @@ if __name__ == "__main__":
     else:
         with open("src/urls.json", "r") as o:
             shortened_urls = json.load(o)
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
